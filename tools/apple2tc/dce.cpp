c9a6665b4388059e3a4f3f14ea49a28f10aca8ab
Tzvetan Mikov <tmikov@gmail.com>
2022-01-17 12:13:05 -0800

    apple2tc: split dce into a separate file

---
+++ tools/apple2tc/dce.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Tzvetan Mikov.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "PubIR.h"
+#include "ir/IR.h"
+
+using namespace ir;
+
+/// Remove instructions with zero users from a block.
+static bool dceBlock(BasicBlock *bb) {
+  InstDestroyer destroyer;
+  bool changed = false;
+  for (auto &iRef : bb->reverse_instructions()) {
+    Instruction *inst = &iRef;
+
+    if (inst->getType()->getKind() == TypeKind::Void)
+      continue;
+    if (inst->hasUsers())
+      continue;
+    // Check for side effects;
+    switch (inst->getKind()) {
+    case ValueKind::Peek8:
+    case ValueKind::Peek16al:
+    case ValueKind::Peek16un:
+      continue;
+    default:
+      break;
+    }
+
+    destroyer.add(inst);
+    changed = true;
+  }
+
+  return changed;
+}
+
+bool dce(Module *mod) {
+  bool changed = false;
+  for (auto &func : mod->functions()) {
+    bool funcChanged;
+    do {
+      funcChanged = false;
+      // FIXME: we need to do this in reverse post order.
+      for (auto &bb : func.basicBlocks()) {
+        funcChanged |= dceBlock(&bb);
+        changed |= funcChanged;
+      }
+    } while (funcChanged);
+  }
+  return changed;
+}
