bf9ce4e4443a82163ecb61e3d6ec018ac006f606
Tzvetan Mikov <tmikov@gmail.com>
2022-01-28 19:25:21 -0800

    decoded/snake-byte: decompile with real subroutines

--- decoded/snake-byte/decompile.sh
+++ decoded/snake-byte/decompile.sh
@@ -3,6 +3,6 @@
 bin=../../cmake-build-debug
 apple2tc=$bin/tools/apple2tc/apple2tc
 
-$apple2tc snake-byte.b33 --run-data=snake-byte.json --asm > snake-byte.lst
-$apple2tc snake-byte.b33 --run-data=snake-byte.json --simple-c > snake-byte.c
-#$apple2tc snake-byte.b33 --run-data=snake-byte.json -O3 --irc1 > snake-bytec1.c
+#$apple2tc snake-byte.b33 --run-data=snake-byte.json --asm > snake-byte.lst
+#$apple2tc snake-byte.b33 --run-data=snake-byte.json --simple-c > snake-byte.c
+$apple2tc snake-byte.b33 --run-data=snake-byte.json -O3 --irc1 -v1 > snake-bytec1.c
