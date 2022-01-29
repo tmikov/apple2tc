580e0c79afc2a1c0c1332e30ee3e381bae7c2793
Tzvetan Mikov <tmikov@gmail.com>
2022-01-28 19:24:22 -0800

    decoded/robotron: decompile using real subroutines

--- decoded/robotron/decompile.sh
+++ decoded/robotron/decompile.sh
@@ -5,5 +5,5 @@ apple2tc=$bin/tools/apple2tc/apple2tc
 
 $apple2tc robotron2084.b33 --run-data=robotron.json --simple-c > robotron.c
 #$apple2tc robotron2084.b33 --run-data=robotron.json --asm > robotron.lst
-#$apple2tc robotron2084.b33 --run-data=robotron.json -O3 --ir > robotronc1.s
-#$apple2tc robotron2084.b33 --run-data=robotron.json -O3 --irc1 > robotronc1.c
+#$apple2tc robotron2084.b33 --run-data=robotron.json -O3 --ir -v2 > robotron.s
+$apple2tc robotron2084.b33 --run-data=robotron.json -O3 --irc1 -v1 > robotronc1.c
